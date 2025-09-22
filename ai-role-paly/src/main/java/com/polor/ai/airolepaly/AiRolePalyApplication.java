06d7fc5888688afd9ac339590b51c11e4632697c
彭浩琛 <peng.haochen@iwhalecloud.com>
2025-09-23 01:03:30 +0800

    first

---
+++ ai-role-paly/src/main/java/com/polor/ai/airolepaly/AiRolePalyApplication.java
@@ -0,0 +1,13 @@
+package com.polor.ai.airolepaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRolePalyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRolePalyApplication.class, args);
+    }
+
+}
